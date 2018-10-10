61f86c3f616fd638f86e53a4767f473ba7828943
mscott13 <a.markscott13@gmail.com>
2018-10-09 23:29:20 -0500

    update

---
+++ WebService/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Net.Http;
+using System.Web.Http.Cors;
+
+namespace WebService.Controllers
+{
+    [EnableCors("*", "*", "*")]
+    public class UserController : ApiController
+    {
+        [HttpPost]
+        public HttpResponseMessage Register()
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public HttpResponseMessage Login()
+        {
+            return null;
+        }
+    }
+}
